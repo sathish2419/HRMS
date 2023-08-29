3a1e6dd22844d8ccbe8a875ed07a3f27cfe4a34b
nishalini2 <114395166+nishalini2@users.noreply.github.com>
2023-08-29 12:51:17 -0700

    commited

--- src/main/java/com/isys/erp/controller/RoleController.java
+++ src/main/java/com/isys/erp/controller/RoleController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v2")
+@RequestMapping("/api/v2")////
 
 public class RoleController {
     @Autowired
