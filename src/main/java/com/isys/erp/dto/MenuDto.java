c9ad19deccc6e501c5d6eb78c6105c13a28b8a10
nandhinic1612 <107404571+nandhinic1612@users.noreply.github.com>
2023-08-30 12:34:40 +0530

    OneToMany Completed

--- src/main/java/com/isys/erp/dto/MenuDto.java
+++ src/main/java/com/isys/erp/dto/MenuDto.java
@@ -1,8 +1,5 @@
 package com.isys.erp.dto;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MenuDto {
-
 
     private Long menuId;
     private String menuName;
